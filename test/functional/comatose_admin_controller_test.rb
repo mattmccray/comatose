65ef4859e1403cbf1baf353c15caba91485606c4
Michael Kovacs <kovacs@gmail.com>
2009-01-08 02:26:37 +0800

    stub out authentication when testing admin controller
    Signed-off-by: M@ McCray <darthapo@gmail.com>

--- test/functional/comatose_admin_controller_test.rb
+++ test/functional/comatose_admin_controller_test.rb
@@ -16,6 +16,8 @@ class ComatoseAdminControllerTest < Test::Unit::TestCase
     @controller = ComatoseAdminController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    Comatose.config.admin_get_author = nil
+    Comatose.config.admin_authorization = nil
   end
   
   should "show the index action" do
